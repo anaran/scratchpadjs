26fcaba42c5f672eb6de618c64fc710c673ef0f2
Adrian Aichner <adrian.aichner@gmail.com>
2013-10-30 02:47:01 +0100

    Make chrome console command history easy to navigate and save as HTML.
    Might be a good candidate for integration into "Devtools Import/Export".

--- collect_command_history.js
+++ collect_command_history.js
@@ -1,26 +1,57 @@
-// snippet collect_command_history.js exported by snippeteer from
-// Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1684.0 Safari/537.36
-// at 2013-10-28T20:19:08.355Z
 (function() {
-    var selectedCommands = [];
-    catchSelectAndClose = function(event) {
-        if (event.ctrlKey && event.keyCode === "A".charCodeAt(0)) {
-            selectedCommands.push(event.target.innerText);
+    try {
+        var alertAndWarn = function(message) {
+            window.alert(message + "\nSee JavaScript console the complete log of warnings");
+            console.warn(message);
         }
-        if (event.ctrlKey && event.shiftKey && event.keyCode === "J".charCodeAt(0)) {
-            if (window.confirm("save selected (Ctrl+A) commands?")) {
-                var a = document.createElement('a');
-                var blob = new window.Blob([JSON.stringify(selectedCommands, null, 4)], {
-                    'type': 'text/utf-8'
-                });
-                var a = document.createElement('a');
-                a.href = URL.createObjectURL(blob);
-                a.download = "console_selected_" + Date.now() + '.txt';
-                var nw = window.open("", "");
-                nw.document.body.appendChild(a);
-                a.click();
+        var selectCommand = function(event, prevent, stop) {
+            if (prevent) {
+                event.preventDefault();
             }
-        }
+            if (stop) {
+                event.stopPropagation();
+            }
+            var selection = window.getSelection();
+            selection.removeAllRanges();
+            var range = document.createRange();
+            range.selectNodeContents(event.target);
+            // Collapse range to end, i.e. toStart argument is false.
+            // range.collapse(false);
+            // Always add the range to restore focus.
+            selection.addRange(range);
+        };
+        var w = window.open("", "");
+        var meta = document.createElement('meta');
+        meta.content = "text/html; charset=UTF-8";
+        meta.httpEquiv = "content-type";
+        w.document.head.appendChild(meta);
+        var ch = JSON.parse(localStorage.consoleHistory);
+        w.document.title = "consoleHistory (" + ch.length + ' commands)';
+        ch.forEach(function(command) {
+            var pre = document.createElement('pre');
+            pre.setAttribute('style', 'display: inline; border: 1px solid silver;');
+            pre.contentEditable = true;
+            pre.addEventListener('mouseenter', selectCommand, true);
+            pre.addEventListener('dragstart', function(event) {
+                event.dataTransfer.effectAllowed = "copy";
+            }, false);
+            pre.addEventListener('drop', function(event) {
+                event.dataTransfer.effectAllowed = "none";
+            }, false);
+            pre.addEventListener('keydown', function(event) {
+                selectCommand(event, false, true)
+            }, true);
+            pre.addEventListener('keypress', function(event) {
+                selectCommand(event, false, true)
+            }, true);
+            pre.addEventListener('keyup', function(event) {
+                selectCommand(event, false, true)
+            }, true);
+            pre.innerText = command;
+            w.document.body.appendChild(document.createElement('pre').appendChild(pre).parentElement);
+            console.log(command);
+        });
+    } catch (exception) {
+        alertAndWarn(exception.stack.replace(/:(\d+):(\d+)/g, "$& (Line $1, Column $2)"));
     }
-    window.addEventListener('keydown', catchSelectAndClose, false);
 })();
