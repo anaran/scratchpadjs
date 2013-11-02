b8d7dbec0e804ca3e383f4007547e6026faba8f8
Adrian Aichner <adrian.aichner@gmail.com>
2013-11-02 13:32:56 +0100

    Convert to run in tab instead of popup.
    Add title attributes to make dimension meanings explorable.
    Update o resize event only.

--- WindowOpen.js
+++ WindowOpen.js
@@ -1,7 +1,7 @@
-// snippet WindowOpen.js exported by snippeteer from
+ // snippet WindowOpen.js exported by snippeteer from
 // Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1684.0 Safari/537.36
 // at 2013-10-28T20:19:08.353Z
- // See https://developer.mozilla.org/en-US/docs/Web/API/window.open
+// See https://developer.mozilla.org/en-US/docs/Web/API/window.open
 // Try this from a New Tab so that popups are not blocked by the browser.
 (function() {
     try {
@@ -34,94 +34,132 @@
             }
             return timeString;
         }
+        function setupTab() {
+            var w = window.open("", "");
+            w.document.title = (new Error()).stack.split('\n')[1].trim();
+            var clock = w.document.body.appendChild(document.createElement('pre'));
+            var clockStyle2 = w.document.body.appendChild(document.createElement('pre'));
+            var clockStyle3 = w.document.body.appendChild(document.createElement('pre'));
+            var clockTimerId;
+            var size = w.document.body.appendChild(document.createElement('pre'));
+            size.title = "outerWidth * outerHeight @ screenX, screenY of original window";
+            var screen = w.document.body.appendChild(document.createElement('pre'));
+            screen.title = "availWidth * availHeight @ availLeft * availTop of screen";
+            var popup = w.document.body.appendChild(document.createElement('pre'));
+            popup.title = "outerWidth * outerHeight @ screenX, screenY of Clock & Window Size Tracker Tab";
+            w.addEventListener('resize', function(event) {
+                screen.textContent = pad(w.screen.availWidth, ' ', 4) + '*' + pad(w.screen.availHeight, ' ', 4) + '@' + pad(w.screen.availLeft, ' ', 4) + '*' + pad(w.screen.availTop, ' ', 4);
+                popup.textContent = pad(w.outerWidth, ' ', 4) + '*' + pad(w.outerHeight, ' ', 4) + '@' + pad(w.screenX, ' ', 4) + ',' + pad(w.screenY, ' ', 4);
+            }, false);
+            window.addEventListener('resize', function(event) {
+                size.textContent = pad(window.outerWidth, ' ', 4) + '*' + pad(window.outerHeight, ' ', 4) + '@' + pad(window.screenX, ' ', 4) + ',' + pad(window.screenY, ' ', 4);
+                screen.textContent = pad(window.screen.availWidth, ' ', 4) + '*' + pad(window.screen.availHeight, ' ', 4) + '@' + pad(window.screen.availLeft, ' ', 4) + '*' + pad(window.screen.availTop, ' ', 4);
+            }, false);
+            var setClockValues = function() {
+                var d = new Date;
+                // TODO Here is a sample of date-time formats provided:
+                // 2013-09-22T14:42:56.878Z
+                // 2013/09/22 16:42:56
+                // 2013-09-22T16:42:56+0200
+                clock.textContent = d.toJSON();
+                clockStyle2.textContent = formatDateTime(d);
+                clockStyle3.textContent = formatDateTime(d, true);
+            };
+            var startClock = function() {
+                setClockValues();
+                clockTimerId = w.setInterval(setClockValues, 1000);
+            };
+            var toggleClock = function(event) {
+                if (clockTimerId) {
+                    clockTimerId = w.clearInterval(clockTimerId);
+                    var selection = w.getSelection();
+                    selection.removeAllRanges();
+                    var range = document.createRange();
+                    range.selectNodeContents(event.target);
+                    // Collapse range to end, i.e. toStart argument is false.
+                    // range.collapse(false);
+                    // Always add the range to restore focus.
+                    selection.addRange(range);
+                } else {
+                    startClock();
+                }
+            };
+            startClock();
+            clock.addEventListener('click', toggleClock, false);
+            clockStyle2.addEventListener('click', toggleClock, false);
+            clockStyle3.addEventListener('click', toggleClock, false);
+            var pad = function(value, padChar, length) {
+                var stringValue = Number(value).toString(10);
+                for (var i = 0, padLength = length - stringValue.length; i < padLength; i++) {
+                    stringValue = padChar + stringValue;
+                }
+                return stringValue;
+            };
+            console.log((new Error()).stack.split('\n')[1].trim());
+        }
         // Minimum is around 148 x 66 due to title, minimize, maximize, close buttons, locationbar
         // canary chrome does not seem to accept any of the Toolbar and chrome features
         // var w = window.open("", "", "width=150,height=100,top=0,left=0,location=0,menubar=0,resizable=0,status=0");
-        var w = window.open("", "", "width=220,height=200,top=10,left=10");
-        w.document.title = (new Error()).stack.split('\n')[1].trim();
-        var clock = w.document.body.appendChild(document.createElement('pre'));
-        var clockStyle2 = w.document.body.appendChild(document.createElement('pre'));
-        var clockStyle3 = w.document.body.appendChild(document.createElement('pre'));
-        var clockTimerId;
-        var size = w.document.body.appendChild(document.createElement('pre'));
-        var screen = w.document.body.appendChild(document.createElement('pre'));
-        var popup = w.document.body.appendChild(document.createElement('pre'));
-        var setClockValues = function() {
-            var d = new Date;
-            // TODO Here is a sample of date-time formats provided:
-            // 2013-09-22T14:42:56.878Z
-            // 2013/09/22 16:42:56
-            // 2013-09-22T16:42:56+0200
-            clock.textContent = d.toJSON();
-            clockStyle2.textContent = formatDateTime(d);
-            clockStyle3.textContent = formatDateTime(d, true);
-        };
-        var startClock = function() {
-            setClockValues();
-            clockTimerId = window.setInterval(setClockValues, 1000);
-        };
-        var toggleClock = function(event) {
-            if (clockTimerId) {
-                clockTimerId = window.clearInterval(clockTimerId);
-                var selection = window.getSelection();
-                selection.removeAllRanges();
-                var range = document.createRange();
-                range.selectNodeContents(event.target);
-                // Collapse range to end, i.e. toStart argument is false.
-                // range.collapse(false);
-                // Always add the range to restore focus.
-                selection.addRange(range);
-            } else {
-                startClock();
-            }
-        };
-        startClock();
-        clock.addEventListener('click', toggleClock, false);
-        clockStyle2.addEventListener('click', toggleClock, false);
-        clockStyle3.addEventListener('click', toggleClock, false);
-        var pad = function(value, padChar, length) {
-            var stringValue = Number(value).toString(10);
-            for (var i = 0, padLength = length - stringValue.length; i < padLength; i++) {
-                stringValue = padChar + stringValue;
-            }
-            return stringValue;
-        };
-        window.addEventListener('resize', function(event) {
-            size.textContent = pad(window.outerWidth, ' ', 4) + '*' + pad(window.outerHeight, ' ', 4) + '@' + pad(window.screenX, ' ', 4) + ',' + pad(window.screenY, ' ', 4);
-        }, false);
-        window.addEventListener('mouseout', function(event) {
-            screen.textContent = pad(window.screen.availWidth, ' ', 4) + '*' + pad(window.screen.availHeight, ' ', 4) + '@' + pad(window.screen.availLeft, ' ', 4) + '*' + pad(window.screen.availTop, ' ', 4);
-            size.textContent = pad(window.outerWidth, ' ', 4) + '*' + pad(window.outerHeight, ' ', 4) + '@' + pad(window.screenX, ' ', 4) + ',' + pad(window.screenY, ' ', 4);
-            popup.textContent = pad(w.outerWidth, ' ', 4) + '*' + pad(w.outerHeight, ' ', 4) + '@' + pad(w.screenX, ' ', 4) + ',' + pad(w.screenY, ' ', 4);
-        }, false);
-        console.log((new Error()).stack.split('\n')[1].trim());
+        //         var w = window.open("", "", "width=220,height=200,top=10,left=10");
+        var div = document.createElement('div');
+        var aYes = document.createElement('a');
+        aYes.innerText = "Clock & Window Size Tracker";
+        aYes.addEventListener('click', function(event) {
+            setupTab();
+            document.body.removeChild(event.target.parentElement.parentElement);
+        });
+        div.appendChild(document.createElement('div').appendChild(aYes).parentElement);
+        var aNo = document.createElement('a');
+        aNo.innerText = "Cancel";
+        aNo.addEventListener('click', function(event) {
+            document.body.removeChild(event.target.parentElement.parentElement);
+        });
+        div.appendChild(document.createElement('div').appendChild(aNo).parentElement);
+        // FIXME This still opens a popup, not a tab!
+        // a.click();
+        div.setAttribute('style', 'position: fixed; top:50%; left: 50%; border: 0.2em solid silver; background: white;')
+        document.body.appendChild(div);
+        console.log(window.location.href);
+        console.log(document.URL);
+        console.log(document.hasFocus());
+        console.log(window.activeElement);
+        console.log(document.activeElement);
+        if (!document.hasFocus()) {
+            window.alert("This window needs your attetion (see Clock & Window Size Tracker)");
+        }
+// console.log(window.hasFocus());
+        console.log(document.defaultView.location.href);
+    // This confirmer also opens a popup, not a tab!
+    //         if (window.confirm("Start Clock & Window Size Tracker?")) {
+    //             setupTab();
+    //         }
     } catch (exception) {
         window.alert(exception.stack);
     }
 })();
 
 if (false) {
-    window.open('data:application/json,' 
-    + JSON.stringify(window.screen).replace(/([[{,])/g, "$1%0a"), 
+    window.open('data:application/json,'
+    + JSON.stringify(window.screen).replace(/([[{,])/g, "$1%0a"),
     //     'jsonFrame',
-    'window' + Date.now(), 
-    'resizeable=0,top=100, left=100, height=' + window.screen.availHeight / 2 
+    'window' + Date.now(),
+    'resizeable=0,top=100, left=100, height=' + window.screen.availHeight / 2
     + ',width=' + window.screen.availWidth / 2 + ',status=1');
-    
-    window.open('data:application/json,' 
-    + JSON.stringify(window.screen).replace(/([[{,])/g, "$1%0a"), 
-    'window' + Date.now(), 
-    'close=0,modal=1,resizable=0,top=100,left=100,height=' 
-    + window.screen.availHeight / 2 + ',scrollbars=0' 
+
+    window.open('data:application/json,'
+    + JSON.stringify(window.screen).replace(/([[{,])/g, "$1%0a"),
+    'window' + Date.now(),
+    'close=0,modal=1,resizable=0,top=100,left=100,height='
+    + window.screen.availHeight / 2 + ',scrollbars=0'
     + ',width=' + window.screen.availWidth / 2 + ',status=0,location=0,titlebar=1');
-    
-    window.open('data:application/json,' 
-    + JSON.stringify(location).replace(/([[{,])/g, "$1%0a"), 
+
+    window.open('data:application/json,'
+    + JSON.stringify(location).replace(/([[{,])/g, "$1%0a"),
     //     +JSON.stringify(location, null, 4),
-    'jsonFrame', 
-    'resizeable,top=100, left=100, height=' + window.screen.availHeight / 2 
+    'jsonFrame',
+    'resizeable,top=100, left=100, height=' + window.screen.availHeight / 2
     + ',width=' + window.screen.availWidth / 2 + ',status=1');
-    
+
     b = document.querySelector('body');
     // FIXME!
     // console.clear();
