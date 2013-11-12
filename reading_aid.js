bebf9c3675b4b038ff4811a999ff5b0263d06695
Adrian Aichner <adrian.aichner@gmail.com>
2013-11-12 23:50:55 +0100

    Works well enough to be useful by page and line.

--- reading_aid.js
+++ reading_aid.js
@@ -1,47 +1,78 @@
-// snippet reading_aid.js exported by snippeteer from
-// Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1703.0 Safari/537.36
-// at 2013-11-09T15:05:08.831Z
 (function() {
-    var lastCBR = document.body.getBoundingClientRect();
-    var lastStyle = window.getComputedStyle(document.body);
+    var lastScrollTop = document.documentElement.scrollTop;
+    var lastStyle = window.getComputedStyle(document.documentElement);
     var div = document.createElement('div');
     div.style.position = "absolute";
     div.style.background = lastStyle.color;
     div.style.color = lastStyle.backgroundColor;
-//     div.innerHTML = "&DownTeeArrow;";
     div.style.fontSize = "xx-large";
     div.title = "reading aid";
     div.style.padding = "3px";
     div.style.borderRadius = "3px";
     div.style.opacity = 0.2;
-    //     div.style.height = window.innerHeight + "px";
-    //     div.style.width = window.innerWidth + "px";
     div.style.left = (window.scrollX + window.innerWidth) + "px";
     div.style.top = window.scrollY + "px";
     div.className = "readingAid";
-    //     document.body.appendChild(div);
+    if (div.parentElement) {
+        document.body.removeChild(div);
+    }
     window.addEventListener('scroll', function(event) {
-        console.log(div);
         if (div.parentElement) {
             document.body.removeChild(div);
         }
-        //         var appendedDiv = document.querySelector('div.readingAid');
-        //         if (appendedDiv) {
-        //             document.body.removeChild(appendedDiv);
-        //         }
-        div.style.left = (window.innerWidth / 2- lastCBR.left) + "px";
-        if (-lastCBR.top < window.scrollY) {
-            div.style.top = (window.innerHeight - lastCBR.top) + "px";
-            div.innerHTML = "&DownTeeArrow;";
+        div.style.left = document.documentElement.scrollLeft + document.documentElement.clientWidth / 2 + "px";
+        // TODO Please note this is not possible in a clean way, see
+        // http://stackoverflow.com/questions/4770025/how-to-disable-scrolling-temporarily
+        //             event.preventDefault();
+        //             event.stopPropagation();
+        var scrollingUp = lastScrollTop > document.documentElement.scrollTop;
+        if (Math.abs(lastScrollTop - document.documentElement.scrollTop) > document.documentElement.clientHeight * 0.5) {
+            // TODO Please note we are scrolling by page here.
+            if (scrollingUp) {
+                div.innerHTML = "&UpTeeArrow;";
+                //                 div.style.top = (lastScrollTop - document.documentElement.clientHeight) + "px";
+                div.style.top = (lastScrollTop) + "px";
+            } else {
+                div.innerHTML = "&DownTeeArrow;";
+                div.style.top = (lastScrollTop + document.documentElement.clientHeight) + "px";
+            }
         } else {
-            div.style.top = -lastCBR.top + "px";
-            div.innerHTML = "&UpTeeArrow;";
+            // TODO Please note we are scrolling by line here.
+            if (scrollingUp) {
+                div.innerHTML = "&UpTeeArrow;";
+                div.style.top = (document.documentElement.scrollTop) + "px";
+            } else {
+                div.innerHTML = "&DownTeeArrow;";
+                div.style.top = (document.documentElement.scrollTop) + "px";
+            }
+
         }
+        //             div.style.top = (window.innerHeight - lastCBR.top) + "px";
+        //             } else {
+        //             div.style.top = (window.scrollY) + "px";
+        //             }
+        //         } else {
+        //             div.style.top = -lastCBR.top + "px";
+        //             div.style.top = (document.documentElement.scrollTop + document.documentElement.clientHeight) + "px";
+        //         }
+        //     }
+        //             return true;
+        // event.returnValue is deprecated. Please use the standard event.preventDefault() instead.
+        //             event.returnValue = false;
+        //         } else {
+        //             if (-lastCBR.top < window.scrollY) {
+        //                 div.style.top = (window.innerHeight - lastCBR.top) + "px";
+        //                 div.innerHTML = "&DownTeeArrow;";
+        //             } else {
+        //                 div.style.top = -lastCBR.top + "px";
+        //                 div.innerHTML = "&UpTeeArrow;";
+        //             }
+        //         }
         document.body.appendChild(div);
         //         window.scrollTo(div.getBoundingClientRect().left, div.getBoundingClientRect().top);
-        //         console.log(JSON.stringify(lastCBR, null, 2));
-        lastCBR = document.body.getBoundingClientRect();
-    //         console.log(JSON.stringify(lastCBR, null, 2));
-    //         console.count(lastCBR);
-    });
+        console.log(JSON.stringify(document.documentElement, [
+            'clientHeight', 'clientLeft', 'clientTop', 'clientWidth',
+            'scrollHeight', 'scrollLeft', 'scrollTop', 'scrollWidth'], 2));
+        lastScrollTop = document.documentElement.scrollTop;
+    }, true);
 })();
