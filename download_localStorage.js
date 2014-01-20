1d7ecbf444ec2d5dbd741f8faad54abddef8da0b
Adrian Aichner <adrian.aichner@gmail.com>
2014-01-20 02:48:51 +0100

    Encode location.origin and locale date string in download file name.

--- download_localStorage.js
+++ download_localStorage.js
@@ -7,10 +7,33 @@
     });
     var a = document.createElement('a');
     a.href = URL.createObjectURL(blob);
-    a.download = "localStorage_" + (new Date()).toJSON().replace(/:/g, '') + '.txt';
-    a.innerText = 'Download localStorage';
+    a.download = "localStorage_" + encodeURIComponent(location.origin) + '@' + Date.now() + '.txt';
+    a.textContent = 'Download localStorage';
 //     a.click();
-    var w = window.open('', '', "width=280,height=100,top=100,left=100");
-    w.document.title = 'localStorage';
-    w.document.body.appendChild(a);
+    // var w = window.open('', '', "width=280,height=100,top=100,left=100");
+    // w.document.title = 'localStorage';
+    // w.document.body.appendChild(a);
+        var myDocument = document;
+        var autosaveIndicator = myDocument.createElement('div');
+        autosaveIndicator.style.position = 'fixed';
+        autosaveIndicator.style.top = '20px';
+        autosaveIndicator.style.left = '20px';
+//        autosaveIndicator.style.bottom = '1em';
+//        autosaveIndicator.style.right = '1em';
+        autosaveIndicator.style.backgroundColor = 'white';
+        autosaveIndicator.style.border = '1px dashed';
+        // autosaveIndicator.style.transition = 'opacity 1s 0s';
+
+        // var downloadLink = myDocument.createElement('pre');
+        // downloadLink.textContent = document.URL + '\n' + document.title + '\n';
+        autosaveIndicator.appendChild(a);
+
+        var close = autosaveIndicator.appendChild(myDocument.createElement('span'));
+        close.textContent = "[x]";
+        close.addEventListener('click', function(event) {
+            event.preventDefault();
+                myDocument.body.removeChild(autosaveIndicator);
+        }, false);
+        myDocument.body.appendChild(autosaveIndicator);
+
 })();
