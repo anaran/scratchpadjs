4dd1dce162d358c8f524c73e4b557d5796d5a347
Adrian Aichner <adrian.aichner@gmail.com>
2014-02-19 22:05:49 +0100

    Support regexp syntax, add usage function, print it via console.info.
    Add NON_NLS markers.
    Need to use "NLS FODDER" so that String Xtrnlzr will add markers to other
    strings.

--- replaceInActiveElement.js
+++ replaceInActiveElement.js
@@ -1,30 +1,31 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
-let replaceInActiveElement = function (search_re, flags, replacement) {
-    var ae = document.activeElement;
-    if (!search_re) {
-        search_re = window.prompt('Search RegExp (please escape \\.\\+\\*\\(\\)\\? etc.', 'e.g. [\\w+\\d]');
+/* jslint browser: true */
+/*global console: false*/
+"use strict"; //$NON-NLS-0$
+var replaceInActiveElement = function(regexp, replacement, element) {
+    var ae = element || document.activeElement;
+    if (regexp === undefined) {
+        var str = window.prompt('Search RegExp (please escape .+*(){}[]? with \\', 'e.g. [\\w+\\d]'); //$NON-NLS-0$
+        var captureGroups = str.match(/^\/?(.+?)(?:\/([gim]*))?$/);
+        var regexp = new RegExp(captureGroups[1], captureGroups[2]);
     }
-    if (!flags) {
-        flags = window.prompt('Flags', 'e.g. [gi] for global, igoring letter case');
-    }
-    if (!replacement) {
-        replacement = window.prompt('Replacement String', 'e.g. [$1] for first matched group');
+    if (replacement === undefined) {
+        replacement = window.prompt('Replacement String with substitutions $1,$&,$`,$', 'e.g. $&'); //$NON-NLS-1$ //$NON-NLS-0$
     }
     if (ae) {
         if (ae.value) {
-            ae.value = ae.value.replace(new RegExp(search_re, flags), replacement);
+            ae.value = ae.value.replace(regexp, replacement);
         }
         if (ae.textContent) {
-            ae.textContent = ae.textContent.replace(new RegExp(search_re, flags), replacement);
+            ae.textContent = ae.textContent.replace(regexp, replacement);
         }
     }
-}
-// replaceInActiveElement('apa\\.selfhost\\.eu', 'g', 'my.own.server');
-replaceInActiveElement();
+};
+
+    function usage() {
+        // Replace globally in currently active Element:
+        replaceInActiveElement(/apa\.selfhost\.eu/g, 'my.own.server'); //$NON-NLS-0$
+        // Replace interactively, prompting for regexp and replacement:
+        replaceInActiveElement();
+    }
+
+console.info(usage.toString());
