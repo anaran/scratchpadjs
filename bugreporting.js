4df888ed554c687537be085f82508dd20852cf88
anaran <adrian.aichner@gmail.com>
2014-02-26 15:09:44 +0100

    Expressions often used in browser bug reporting.

---
+++ bugreporting.js
@@ -0,0 +1,23 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+// Useful information about content window and navigator
+
+document.lastModified
+/*
+02/25/2014 16:46:59
+*/
+navigator.userAgent
+/*
+Mozilla/5.0 (Windows NT 5.1; rv:30.0) Gecko/20100101 Firefox/30.0
+*/
+navigator.buildID
+/*
+20140224030203
+*/
