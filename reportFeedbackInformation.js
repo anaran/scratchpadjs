0a8b27b41c9ee2c96e2c31002f089337fc61fe75
Adrian Aichner <adrian.aichner@gmail.com>
2014-08-24 18:24:49 +0200

    Generate bug reports for MDN and AMO pages based on current selections.
    Multiple selection ranges are supported.
    
    Any href attributes of parentElement are listed in the bug comment.
    URL field is set from document.URL.
    Just adjust the summary line and comment block.
    Review product and component and submit the issue.

--- reportFeedbackInformation.js
+++ reportFeedbackInformation.js
@@ -7,42 +7,69 @@
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
 var reportFeedbackInformation = function () {
-    var copyright = document.querySelector('meta[name=copyright]'),
-    keywords = document.querySelector('meta[name=keywords]'),
-    description = document.querySelector('meta[name=description]'),
-    author = document.querySelector('meta[name=author]'),
-    generator = document.querySelector('meta[name=generator]')
-    known_origins = {
-        "https://developer.mozilla.org": {
-            "help": "https://developer.mozilla.org/en-US/docs/MDN/About#Documentation_errors",
-            "report": "https://bugzilla.mozilla.org/form.doc?bug_file_loc="
-        }
+  var copyright = document.querySelector('meta[name=copyright]'),
+  keywords = document.querySelector('meta[name=keywords]'),
+  description = document.querySelector('meta[name=description]'),
+  author = document.querySelector('meta[name=author]'),
+  generator = document.querySelector('meta[name=generator]')
+  known_origins = {
+    'https://developer.mozilla.org': {
+      'help': 'https://developer.mozilla.org/en-US/docs/MDN/About#Documentation_errors',
+//       'report': 'https://bugzilla.mozilla.org/form.doc?bug_file_loc='
+      'report': 'https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&product=Developer%20Documentation&component=General&short_desc=Suggested%20Solution%20or%20Problem%20Statement'
+    },
+    'https://addons.mozilla.org': {
+      'help': 'https://addons.mozilla.org/en-US/developers/docs/policies/contact',
+      'report': 'https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&product=addons.mozilla.org&short_desc=Suggested%20Solution%20or%20Problem%20Statement'
     }
-    var mailtos = [];
-    // TODO Please see http://www.w3.org/TR/2011/REC-css3-selectors-20110929/#attribute-substrings
-    Array.prototype.forEach.call(document.querySelectorAll('a[href^="mailto:"]'), function (value) {
-        mailtos.push(value.href);
-    });
-    var gpluses = [];
-    Array.prototype.forEach.call(document.querySelectorAll('a[href^="https://plus.google.com/"]'), function (value) {
-        gpluses.push(value.href);
-    });
-    var data = {
-        copyright: copyright && copyright.content,
-        keywords: keywords && keywords.content,
-        description: description && description.content,
-        author: author && author.content,
-        generator: generator && generator.content,
-        mailtos: mailtos,
-        gpluses: gpluses,
-        known_origins: known_origins
-    };
-    var s = window.getSelection();
-    var brokenLink = s && (s.anchorNode.parentElement && s.anchorNode.parentElement.href || s.focusNode.parentElement && s.focusNode.parentElement.href);
-    // window.alert('potential feedback information\n' + JSON.stringify(data, null, 2));
-    if (brokenLink && known_origins[window.location.origin]) {
-        window.open(known_origins[window.location.origin].report + window.location.href + " Broken link " + brokenLink + " in " + window.location.href);
+  }
+  var mailtos = [
+  ];
+  // TODO Please see http://www.w3.org/TR/2011/REC-css3-selectors-20110929/#attribute-substrings
+  Array.prototype.forEach.call(document.querySelectorAll('a[href^="mailto:"]'), function (value) {
+    mailtos.push(value.href);
+  });
+  var gpluses = [
+  ];
+  Array.prototype.forEach.call(document.querySelectorAll('a[href^="https://plus.google.com/"]'), function (value) {
+    gpluses.push(value.href);
+  });
+  var s = window.getSelection();
+  let rangeLinks = [
+  ];
+  for (let i = 0; i < s.rangeCount; i++) {
+    if (s.getRangeAt(i).commonAncestorContainer.parentElement.hasAttribute('href')) {
+      rangeLinks.push(s.getRangeAt(i).commonAncestorContainer.parentElement.href);
     }
-    // window.alert('mailto addresses\n' + JSON.stringify(mailtos, null, 2));
+  }
+  var data = {
+    copyright: copyright && copyright.content,
+    keywords: keywords && keywords.content,
+    description: description && description.content,
+    author: author && author.content,
+    generator: generator && generator.content,
+    mailtos: mailtos,
+    gpluses: gpluses,
+    rangeLinks: rangeLinks,
+    url: document.URL,
+    selection: s
+  };
+  if (known_origins[window.location.origin]) {
+    window.open(known_origins[window.location.origin].report + '&comment='
+                + window.encodeURIComponent((rangeLinks.length ? 'See these links:\n\n'
+                                                            + rangeLinks.join(' ') + '\n\n  referenced from\n\n' : 'See:\n\n') + window.location.href + '\n\nDetails:\n\n' + s.toString())
+                + '&bug_file_loc=' + window.encodeURIComponent(window.location.href)
+    /* , "_blank", 'width=230,height=280,top=10,left=10'*/
+    );
+  }
+  // window.alert('potential feedback information\n' + JSON.stringify(data, null, 2));
+  // window.alert('mailto addresses\n' + JSON.stringify(mailtos, null, 2));
+
 };
 reportFeedbackInformation();
+
+/*
+Exception: s.anchorNode is null
+reportFeedbackInformation@Scratchpad/4:44:7
+@Scratchpad/4:70:1
+*/
