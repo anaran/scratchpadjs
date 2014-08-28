d45145e3941193eef6df099af468887c6747883a
Adrian Aichner <adrian.aichner@gmail.com>
2014-08-28 23:11:42 +0200

    Generate AMO and MDN bug reports pre-populated with links.
    Links are extracted from text selections, is any.
    known_sites easily supports aliases, like allizom for mozilla.
    A window.alert is shown when run on an unknown site.

--- reportFeedbackInformation.js
+++ reportFeedbackInformation.js
@@ -7,21 +7,25 @@
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
 var reportFeedbackInformation = function () {
-  var copyright = document.querySelector('meta[name=copyright]'),
+  let copyright = document.querySelector('meta[name=copyright]'),
   keywords = document.querySelector('meta[name=keywords]'),
   description = document.querySelector('meta[name=description]'),
   author = document.querySelector('meta[name=author]'),
-  generator = document.querySelector('meta[name=generator]')
+  generator = document.querySelector('meta[name=generator]'),
+  mdn = {
+    'help': 'https://developer.mozilla.org/en-US/docs/MDN/About#Documentation_errors',
+    //       'report': 'https://bugzilla.mozilla.org/form.doc?bug_file_loc='
+    'report': 'https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&product=Developer%20Documentation&component=General&short_desc=Suggested%20Solution%20or%20Problem%20Statement'
+  },
+  amo = {
+    'help': 'https://addons.mozilla.org/en-US/developers/docs/policies/contact',
+    'report': 'https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&product=addons.mozilla.org&short_desc=Suggested%20Solution%20or%20Problem%20Statement'
+  }
   known_origins = {
-    'https://developer.mozilla.org': {
-      'help': 'https://developer.mozilla.org/en-US/docs/MDN/About#Documentation_errors',
-//       'report': 'https://bugzilla.mozilla.org/form.doc?bug_file_loc='
-      'report': 'https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&product=Developer%20Documentation&component=General&short_desc=Suggested%20Solution%20or%20Problem%20Statement'
-    },
-    'https://addons.mozilla.org': {
-      'help': 'https://addons.mozilla.org/en-US/developers/docs/policies/contact',
-      'report': 'https://bugzilla.mozilla.org/enter_bug.cgi?format=__default__&product=addons.mozilla.org&short_desc=Suggested%20Solution%20or%20Problem%20Statement'
-    }
+    'https://developer.mozilla.org': mdn,
+    'https://addons.mozilla.org': amo,
+    // staging site for AMO
+    'https://addons.allizom.org': amo
   }
   var mailtos = [
   ];
@@ -56,20 +60,25 @@ var reportFeedbackInformation = function () {
   };
   if (known_origins[window.location.origin]) {
     window.open(known_origins[window.location.origin].report + '&comment='
-                + window.encodeURIComponent((rangeLinks.length ? 'See these links:\n\n'
-                                                            + rangeLinks.join(' ') + '\n\n  referenced from\n\n' : 'See:\n\n') + window.location.href + '\n\nDetails:\n\n' + s.toString())
-                + '&bug_file_loc=' + window.encodeURIComponent(window.location.href)
+    + window.encodeURIComponent((rangeLinks.length ? 'See these links:\n\n'
+    + rangeLinks.join(' ') + '\n\n  referenced from\n\n' : 'See:\n\n') + window.location.href + '\n\nDetails:\n\n' + s.toString())
+    + '&bug_file_loc=' + window.encodeURIComponent(window.location.href)
     /* , "_blank", 'width=230,height=280,top=10,left=10'*/
     );
+  } else {
+    window.alert('potential feedback information\n' + JSON.stringify(data, null, 2));
   }
-  // window.alert('potential feedback information\n' + JSON.stringify(data, null, 2));
   // window.alert('mailto addresses\n' + JSON.stringify(mailtos, null, 2));
 
 };
 reportFeedbackInformation();
-
 /*
 Exception: s.anchorNode is null
 reportFeedbackInformation@Scratchpad/4:44:7
 @Scratchpad/4:70:1
+*/
+/*
+Exception: i is not defined
+reportFeedbackInformation@Scratchpad/1:40:18
+@Scratchpad/1:70:1
 */
