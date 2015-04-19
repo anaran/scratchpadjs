424a3f4f2d4e2c7df3f6ce723652704255d13027
Adrian Aichner <adrian.aichner@gmail.com>
2015-04-19 20:47:58 +0200

    Add missing semicolons.
    Indent in scratchpad.

--- getElementPath.js
+++ getElementPath.js
@@ -4,17 +4,17 @@ NetUtil: false, URL: false, console: false */
 'use strict';
 // Normalize comments because Format JS cannot do that yet.
 // From:^(\s*//\s)(\s+):To:$1:
-var getElementPath = function getElementPath(element, path) {
-    if (!path) {
-        path = '';
-    }
-    // console.trace();
+;var getElementPath = function getElementPath(element, path) {
+  if (!path) {
+    path = '';
+  }
+  // console.trace();
 
-    if (!element) {
-        return path;
-    } else {
-        var elementSelector = element.localName + (element.id ? '#' + element.id : '') + (element.className.length ? '.' + element.className.replace(/ /g, '.')  : '');
-        return getElementPath(element.parentElement, elementSelector + ' ' + path);
-    }
+  if (!element) {
+    return path;
+  } else {
+    var elementSelector = element.localName + (element.id ? '#' + element.id : '') + (element.className.length ? '.' + element.className.replace(/ /g, '.')  : '');
+    return getElementPath(element.parentElement, elementSelector + ' ' + path);
+  }
 };
 exports.getElementPath = getElementPath;
