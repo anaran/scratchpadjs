952c47da8b075606ef4dd08c52cbd4e9fc49eb1b
Adrian Aichner <adrian.aichner@gmail.com>
2015-09-06 20:02:46 +0200

    Add .getTimezoneOffset, add bugzilla textarea and input data, add close button.

--- bugreporting.js
+++ bugreporting.js
@@ -15,7 +15,8 @@
     'navigator.javaEnabled()',
     'navigator.buildID',
     'navigator.language',
-    'navigator.languages'
+    'navigator.languages',
+    '(new Date).getTimezoneOffset()'
   ].sort().map(function (value, index, object) {
     var dt = doc.createElement('dt');
     var dd = doc.createElement('dd');
@@ -28,17 +29,42 @@
       'value': eval(value)
     };
   });
+  // Extract input values from bugzilla from for later use (e.g. when
+  // sbmit fails):
+  Array.prototype.forEach.call(
+    document.querySelectorAll('textarea,input'), function (node) {
+      if (node.value.length && node.id != node.value) {
+        var dt = doc.createElement('dt');
+        var dd = doc.createElement('dd');
+        dt.textContent = node.id;
+        dd.textContent = node.value;
+        dl.appendChild(dt);
+        dl.appendChild(dd);
+        console.log(node.value);
+      }
+    });
   // console.table(table);
   // console.log(dl.textContent);
   // console.log(dl.innerHTML);
-  doc.body.appendChild(dl);
+  var close = doc.createElement('div');
+  // close.textContent = 'Close \u2717 \u2A2F';
+  close.textContent = '\u2A2F';
+  close.onclick = function () {
+    doc.body.removeChild(this.parentElement);
+  };
+  close.style = 'background: white; position: fixed; left: 50%; top: 50%; font-size: large;';
+  // dl.style = 'padding: 1em; top: 2em;';
+  var div = doc.createElement('div');
+  div.appendChild(close);
+  div.appendChild(dl);
+  doc.body.appendChild(div);
   // dl.style = 'position: fixed; background: skyblue;';
   var bcr = dl.getBoundingClientRect();
   console.log(dl.style);
   // dl.style = dl.style + ' top: calc(50% - ' + bcr.height + 'px); left: calc(50% - ' + bcr.width + 'px);';
   // dl.style = 'position: fixed; top: calc(50% - ' + bcr.height + 'px); left: calc(50% - ' + bcr.width + 'px); background: skyblue;';
   // dl.style = 'position: fixed; top: calc(50% - ' + bcr.height + '); left: calc(50% - ' + bcr.width + '); background: skyblue;';
-  dl.style = 'padding: 1em; position: fixed; top: 50%; left: 50%; background: skyblue;';
+  div.style = 'padding: 1em; position: fixed; top: 50%; left: 50%; bottom: 0; right: 0; background: skyblue; overflow: scroll;';
   console.log(bcr);
 }) ();
 // window.alert(window.hasOwnProperty('Cu'));
